e5612c98663bbb25478df1b9b42cfc4de9341c50
Luigino Perez <luigino.mp@gmail.com>
2019-01-18 14:34:27 -0500

    Created Question 3 Project
    Set up classes - need to implement all functionality

--- Question2/src/test/java/com/revature/q2/test/Q2Test.java
+++ Question2/src/test/java/com/revature/q2/test/Q2Test.java
@@ -4,20 +4,26 @@ import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
 import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.revature.q2.Q2Mapper;
+import com.revature.q2.Q2Reducer;
  
 public class Q2Test {
 	  MapDriver<LongWritable, Text, Text, DoubleWritable>  mapDriver;
 	  ReduceDriver <Text, DoubleWritable, Text, DoubleWritable> reduceDriver;
+	  MapReduceDriver<LongWritable, Text, Text, DoubleWritable, Text, DoubleWritable> mapReduceDriver;
 	  
 	  @Before
 	  public void setUp() {
 		    Q2Mapper mapper = new Q2Mapper();
+		    Q2Reducer reducer = new Q2Reducer();
 		    mapDriver = MapDriver.newMapDriver(mapper);
+		    reduceDriver = ReduceDriver.newReduceDriver(reducer);
+		    mapReduceDriver = MapReduceDriver.newMapReduceDriver(mapper, reducer);
 	  }
 	 
 	  @Test
---
+++ Question3/src/main/java/com/revature/q3/Q3Mapper.java
@@ -0,0 +1,10 @@
+package com.revature.q3;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class Q3Mapper extends Mapper <LongWritable, Text, Text, DoubleWritable> {
+
+}
